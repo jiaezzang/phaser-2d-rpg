de1ff50da29c5675364822d5b64ebf7bdce1a6e7
jiaezzang <visveryver2@gmail.com>
2023-11-06 18:04:09 +0900

    진행상황 공유

--- src/scenes/DisplayScene.ts
+++ src/scenes/DisplayScene.ts
@@ -1,23 +1,54 @@
-import { Background } from '../Background';
+import { Background } from "../Background";
+import Player from "../components/Player";
+export function makeTileLayer(
+  map: Phaser.Tilemaps.Tilemap,
+  tilesetName: string,
+  assetKey: string,
+  layerId: string
+) {
+  const tileset = map.addTilesetImage(tilesetName, assetKey);
 
+  return map.createLayer(layerId, tileset);
+}
 export default class DisplayScene extends Phaser.Scene {
-    background!: Background;
-    constructor() {
-        super({ key: 'display' });
-        console.log('cons! s');
-    }
-    init() {
-        console.log('init s');
-    }
-    preload() {
-        console.log('preload s');
-    }
-    create() {
-        this.background = new Background(this);
-        console.log('create! s');
-    }
-    update(time: number, delta: number): void {
-        console.log('update s');
-        this.background.update();
-    }
+  player!: Player;
+  background!: Background;
+  cursors!: Phaser.Types.Input.Keyboard.CursorKeys;
+  tileMap!: Phaser.GameObjects.TileSprite;
+  constructor() {
+    super({ key: "display" });
+    console.log("cons! s");
+  }
+  init() {
+    console.log("init s");
+  }
+  preload() {
+    console.log("preload s");
+  }
+  create() {
+    this.background = new Background(this);
+    this.player = new Player(this, 0, 800, "player", "stand1");
+    console.log("create! s");
+
+    /** keyboard, input */
+    this.cursors = this.input.keyboard.createCursorKeys();
+
+    //map
+    const map = this.make.tilemap({ key: "map" });
+    console.log(map);
+    // const tileset = map.addTilesetImage("map", "platforms");
+    const platformsLayer = makeTileLayer(map, "map", "platforms", "platforms");
+
+    console.log(platformsLayer);
+    // if (tileset) {
+    //   console.log("complete");
+    //   const platforms = map.createLayer("platforms", tileset);
+    //   platforms?.setCollisionByExclusion([-1]);
+    //   if (platforms) this.physics.add.collider(this.player, platforms);
+    // }
+  }
+  update(time: number, delta: number): void {
+    this.background.update();
+    this.player.update(this.cursors);
+  }
 }
--- src/scenes/Preloader.ts
+++ src/scenes/Preloader.ts
@@ -1,12 +1,17 @@
 export default class Preloader extends Phaser.Scene {
-    constructor() {
-        super({ key: 'preloader' });
-    }
-    preload() {
-        this.load.setBaseURL('assets');
-        this.load.image('bg1', 'background/bg1.png');
-    }
-    create() {
-        this.game.scene.start('display');
-    }
+  constructor() {
+    super({ key: "preloader" });
+  }
+  preload() {
+    this.load.setBaseURL("assets");
+    this.load.image("bg1", "background/bg1.png");
+    this.load.atlas("player", "player/poy.png", "player/poy.json");
+
+    //tile
+    this.load.image("platforms", "map/spritesheet_jumper.png");
+    this.load.tilemapTiledJSON("map", "map/map.json");
+  }
+  create() {
+    this.game.scene.start("display");
+  }
 }
