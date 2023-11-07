829f270b61adcba347a3caa1a2543b517effa6de
jiaezzang <visveryver2@gmail.com>
2023-11-07 10:02:07 +0900

    map.json 파일 교체

--- src/scenes/DisplayScene.ts
+++ src/scenes/DisplayScene.ts
@@ -1,20 +1,11 @@
 import { Background } from "../Background";
 import Player from "../components/Player";
-export function makeTileLayer(
-  map: Phaser.Tilemaps.Tilemap,
-  tilesetName: string,
-  assetKey: string,
-  layerId: string
-) {
-  const tileset = map.addTilesetImage(tilesetName, assetKey);
-
-  return map.createLayer(layerId, tileset);
-}
 export default class DisplayScene extends Phaser.Scene {
   player!: Player;
   background!: Background;
   cursors!: Phaser.Types.Input.Keyboard.CursorKeys;
   tileMap!: Phaser.GameObjects.TileSprite;
+  platformsLayer!: Phaser.Tilemaps.TilemapLayer;
   constructor() {
     super({ key: "display" });
     console.log("cons! s");
@@ -26,6 +17,7 @@ export default class DisplayScene extends Phaser.Scene {
     console.log("preload s");
   }
   create() {
+    this.load.start()
     this.background = new Background(this);
     this.player = new Player(this, 0, 800, "player", "stand1");
     console.log("create! s");
@@ -36,16 +28,17 @@ export default class DisplayScene extends Phaser.Scene {
     //map
     const map = this.make.tilemap({ key: "map" });
     console.log(map);
-    // const tileset = map.addTilesetImage("map", "platforms");
-    const platformsLayer = makeTileLayer(map, "map", "platforms", "platforms");
+    //Tileset이 생성되지 않음
+    const tileset = map.addTilesetImage("map", "platforms");
+    console.log(tileset);
 
-    console.log(platformsLayer);
-    // if (tileset) {
-    //   console.log("complete");
-    //   const platforms = map.createLayer("platforms", tileset);
-    //   platforms?.setCollisionByExclusion([-1]);
-    //   if (platforms) this.physics.add.collider(this.player, platforms);
-    // }
+    if (tileset) {
+      console.log("complete");
+      const platforms = map.createLayer("platforms", tileset);
+      if (platforms) this.platformsLayer = platforms ?? '';
+      platforms?.setCollisionByExclusion([-1]);
+      if (platforms) this.physics.add.collider(this.player, platforms);
+    }
   }
   update(time: number, delta: number): void {
     this.background.update();
