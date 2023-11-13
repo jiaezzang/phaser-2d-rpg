f697de8385258343d83883b2914b0508d58af51c
jiaezzang <visveryver2@gmail.com>
2023-11-14 08:48:29 +0900

    update

--- src/components/healthBar/HealthBar.ts
+++ src/components/healthBar/HealthBar.ts
@@ -18,18 +18,12 @@ export default class HealthBar extends Phaser.GameObjects.Graphics {
 
     this.hpbarImage = scene.add.image(this.x, this.y, "hpbar");
     this.hpbarImage.setScale(0.3).setScrollFactor(0).setDepth(1);
-
     this.draw();
     this.bar.setScrollFactor(0);
     this.bar.setDepth(1);
-
     scene.add.existing(this.bar);
-    // scene.add.existing(hpbarImage);
-
   }
-  create() {
-
-  }
+  create() {}
   draw() {
     this.bar.clear();
 
@@ -40,18 +34,17 @@ export default class HealthBar extends Phaser.GameObjects.Graphics {
     }
 
     var d = Math.floor(this.p * this.value);
-
     this.bar.fillRect(this.x - 82 + 2, this.y + 3, d, 23);
   }
   increaseHP(type: string) {
-    if(type === 'redPotion') this.hp = 60;
-    else if(type === 'purplePotion') this.hp = 180;
+    if (type === "redPotion") this.hp = 60;
+    else if (type === "purplePotion") this.hp = 180;
 
     this.currentHp = this.value;
     this.newHp = this.value + this.hp;
 
     if (this.updateTween && this.updateTween.isPlaying()) {
-      this.updateTween.on('complete', () => {
+      this.updateTween.on("complete", () => {
         this.currentHp = this.newHp;
         this.newHp += this.hp;
         this.updateTween = this.scene.tweens.addCounter({
@@ -59,13 +52,13 @@ export default class HealthBar extends Phaser.GameObjects.Graphics {
           to: this.value + this.hp,
           duration: 500,
           ease: "linear",
-          onUpdate: (tween) => { 
+          onUpdate: (tween) => {
             this.value = Math.round(tween.getValue());
-            if(this.value >= 473) this.value = 473
+            if (this.value >= 473) this.value = 473;
             this.draw();
           },
         });
-      })
+      });
     } else {
       this.updateTween = this.scene.tweens.addCounter({
         from: this.value,
@@ -74,14 +67,11 @@ export default class HealthBar extends Phaser.GameObjects.Graphics {
         ease: "linear",
         onUpdate: (tween) => {
           this.value = Math.round(tween.getValue());
-          if(this.value >= 473) this.value = 473
+          if (this.value >= 473) this.value = 473;
           this.draw();
         },
       });
     }
-    // this.value += hp;
-
-    // this.draw();
   }
   decreaseHp(attak: number) {
     this.value -= attak;
